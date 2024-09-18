e50417c21591e32c1b72f016d6530e085e2e406e
Rakesh60 <rakeshibm909@gmail.com>
2024-09-18 17:16:28 +0530

    Custom Error page added

--- src/main/java/com/vendor/controller/ErrorController.java
+++ src/main/java/com/vendor/controller/ErrorController.java
@@ -1,11 +1,16 @@
 package com.vendor.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
-public class ErrorController {
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController{
 
     @GetMapping("/access-denied")
     public String accessDenied(Model model) {
@@ -19,6 +24,24 @@ public class ErrorController {
     	model.addAttribute("errorMessage", "Oops! The page you are looking for does not exist.");
     	return "error";
     }
-    
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request ,Model M) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+            	M.addAttribute("errorMessage","The Page you are looking not found");
+            	M.addAttribute("title","Error "+ statusCode);
+                return "error"; // Return your custom 404 page
+            }
+        }
+        return "error"; // Generic error page
+    }
+
+    public String getErrorPath() {
+        return "/error";
+    }
     
 }
